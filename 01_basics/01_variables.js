58168293b01099dbfa65ca45b682c849db759dd4
dhruvgmarg-hue <dhruv.gmarg@gmail.com>
2025-09-02 09:25:34 +0000

    commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "dhruv@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+
+// accountId = 100 // Throws error as accountId is a constant
+
+// console.log(accountId);
+
+accountEmail = "hc@hc.com";
+accountPassword = "67890";
+accountCity = "Mumbai";
+console.table({accountEmail, accountPassword, accountCity});
